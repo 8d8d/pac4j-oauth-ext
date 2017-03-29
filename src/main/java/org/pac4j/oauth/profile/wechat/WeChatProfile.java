97071697c8684881ffb97f688a3434b57783e1a1
linzl <2225010489@qq.com>
2017-03-29 13:58:22 +0800

    add method

--- src/main/java/org/pac4j/oauth/profile/wechat/WeChatProfile.java
+++ src/main/java/org/pac4j/oauth/profile/wechat/WeChatProfile.java
@@ -2,7 +2,8 @@
 package org.pac4j.oauth.profile.wechat;
 
 import org.pac4j.core.profile.AttributesDefinition;
-import org.pac4j.oauth.profile.OAuth20Profile;
+import org.pac4j.core.profile.Gender;
+import org.pac4j.oauth.profile.OAuth10Profile;
 
 /**
  * 用于添加返回用户信息
@@ -11,13 +12,46 @@ import org.pac4j.oauth.profile.OAuth20Profile;
  *
  *         2016年12月25日
  */
-public class WeChatProfile extends OAuth20Profile {
+public class WeChatProfile extends OAuth10Profile {
 
 	private static final long serialVersionUID = 6790248892408246089L;
 
 	@Override
 	protected AttributesDefinition getAttributesDefinition() {
 		return new WeChatAttributesDefinition();
+	}
+
+	public String getOpenId() {
+		return (String) getAttribute(WeChatAttributesDefinition.OPEN_ID);
+	}
+
+	public String getNickName() {
+		return (String) getAttribute(WeChatAttributesDefinition.NICK_NAME);
+	}
+
+	public Gender getGender() {
+		final Integer sex = (Integer) getAttribute(WeChatAttributesDefinition.SEX);
+		Gender gender = null;
+		switch (sex) {
+		case 1:
+			gender = Gender.MALE;
+			break;
+		case 2:
+			gender = Gender.FEMALE;
+			break;
+		default:
+			gender = Gender.UNSPECIFIED;
+			break;
+		}
+		return gender;
+	}
+
+	public String getHeadimgurl() {
+		return (String) getAttribute(WeChatAttributesDefinition.HEAD_IMG_URL);
+	}
+
+	public String getUnionid() {
+		return (String) getAttribute(WeChatAttributesDefinition.UNION_ID);
 	}
 
 }
